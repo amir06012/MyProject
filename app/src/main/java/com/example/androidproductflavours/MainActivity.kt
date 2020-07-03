6e08143f30ba8c317102018fe2aba427d3f06d1e
amir06012 <amirali06012@gmail.com>
2020-07-04 01:50:12 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/androidproductflavours/MainActivity.kt
+++ app/src/main/java/com/example/androidproductflavours/MainActivity.kt
@@ -7,11 +7,5 @@ open class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       // hello1
-
-        //hello6
-
-        //hello7
-            //hello8
     }
 }
