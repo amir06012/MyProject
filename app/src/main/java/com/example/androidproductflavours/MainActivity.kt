f5dbdce70db59409570cc01917fdea5f9f5c7af7
amir06012 <amirali06012@gmail.com>
2020-07-03 16:17:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/androidproductflavours/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.androidproductflavours
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+open class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+
+}
