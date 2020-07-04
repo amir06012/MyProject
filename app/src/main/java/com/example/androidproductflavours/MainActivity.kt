ffb5f8e6bad366dfa8e2b6622ba428607c90e927
amir06012 <amirali06012@gmail.com>
2020-07-04 19:19:05 +0530

    commit 673870
    hello text

--- app/src/main/java/com/example/androidproductflavours/MainActivity.kt
+++ app/src/main/java/com/example/androidproductflavours/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.androidproductflavours
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.product.activity_main.*
 
 open class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.text="Hello Amir"
+
     }
 }
